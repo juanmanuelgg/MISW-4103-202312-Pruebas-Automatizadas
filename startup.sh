787672f46fa490cbc67dd1cc9cc9c3fb1fcc2ef7
Juan Manuel González Garzón <jm.gonzalez1844@uniandes.edu.co>
2023-04-01 10:48:50 -0500

    Create startup.sh

---
+++ startup.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+USAGE="Modo de uso: $0 [-g 'x.y.z' -m 'x.y.z' -p '****' ] [-h]      \n
+	-u Ghost version                                                \n
+	-H MySQL version                                                \n
+	-p Ghost port                                                   \n
+    -h Mostrar esta ayuda                                           \n
+"
+
+GHOST_VERSION=
+MYSQL_VERSION=
+GHOST_PORT=
+
+function processInvocation () {
+    while getopts "g:m:p:h" opt; do
+        case ${opt} in
+            g) GHOST_VERSION=${OPTARG};;
+            m) MYSQL_VERSION=${OPTARG};;
+            p) GHOST_PORT=${OPTARG};;
+            h) echo -e ${USAGE}
+                exit 0
+                ;;
+            *) echo -e ${USAGE} 1>&2
+                exit 1
+                ;;
+        esac
+    done
+
+    if [ -z "${GHOST_VERSION}" ] || [ -z "${MYSQL_VERSION}" ] || [ -z "${GHOST_PORT}" ]; then
+		echo -e ${USAGE} 1>&2
+		exit 1
+	fi
+}
+
+function ensureVolumesFolders () {
+    set -x
+    mkdir -p ./docker/volumes/ghost-volumes/${GHOST_VERSION:-latest}
+    mkdir -p ./docker/volumes/mysql-volumes/${MYSQL_VERSION:-latest}
+    set +x
+}
+
+function main () {
+    export GHOST_VERSION
+    export MYSQL_VERSION
+    export GHOST_PORT
+    docker compose up -d --build
+}
+
+processInvocation $@
+ensureVolumesFolders
+main
